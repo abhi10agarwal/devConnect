773b58d067aa76b8352a22674eb7eb8d174ba8ea
Abhishek agarwal <abhishek10agarwal@gmail.com>
2020-06-21 15:52:12 +0530

    adding user authenticate and create token

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const app = express();
+const express = require('express')
+const connectDB = require('./config/db')
+const app = express()
 
 // Connect database
-connectDB();
-app.get('/', (req, res) => res.send('API Running'));
+connectDB()
+app.get('/', (req, res) => res.send('API Running'))
 
-const PORT = process.env.PORT || 5000;
+//Init Middleware
+app.use(express.json({ extended: false }))
 
 //define routes
 
-app.use('/api/users', require('./routes/api/user'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/user'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
