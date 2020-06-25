ccc19460050753781c671fc96f4b90dc74ea589b
Abhishek agarwal <abhishek10agarwal@gmail.com>
2020-06-25 23:32:48 +0530

    adding layout and auth routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React, { fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+
 import Navbar from './components/layout/Navbar'
 import './App.css'
+import Register from './components/auth/Register'
 
 const App = () => (
 	<Router>
 		<fragment>
 			<Navbar />
 			<Route exact path='/' component={Landing} />
+			<section className='container'>
+				<Switch>
+					<Route exact path='/register' component={Register} />
+					<Route exact path='/login' component={Login} />
+				</Switch>
+			</section>
 		</fragment>
 	</Router>
 )
