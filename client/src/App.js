7a36a8999076178da65acd1ed92ffe428aed8284
Abhishek agarwal <abhishek10agarwal@gmail.com>
2020-06-28 02:16:34 +0530

    Creating profile, adding Experience, Education and deletion All

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,12 @@ import Navbar from './components/layout/Navbar'
 import './App.css'
 import Register from './components/auth/Register'
 import Alert from './components/layout/Alert'
-
+import Dashborad from './components/dashboard/Dashboard'
+import CreateProfile from './components/profile-forms/CreateProfile'
+import EditProfile from './components/profile-forms/EditProfile'
+import AddExperience from './components/profile-forms/AddExperience'
+import AddEducation from './components/profile-forms/AddEducation'
+import PrivateRoute from './components/routing/PrivateRoute'
 // Redux
 
 import { Provider } from 'react-redux'
@@ -34,6 +39,27 @@ const App = () => {
 						<Switch>
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
+							<PrivateRoute exact path='/dashboard' component={Dashborad} />
+							<PrivateRoute
+								exact
+								path='/create-profile'
+								component={CreateProfile}
+							/>
+							<PrivateRoute
+								exact
+								path='/edit-profile'
+								component={EditProfile}
+							/>
+							<PrivateRoute
+								exact
+								path='/add-experience'
+								component={AddExperience}
+							/>
+							<PrivateRoute
+								exact
+								path='/add-education'
+								component={AddEducation}
+							/>
 						</Switch>
 					</section>
 				</Fragment>
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const request = require('request')
 const config = require('config')
 const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator')
+const Post = require('../../models/Post')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 
@@ -135,6 +136,8 @@ router.get('/user/:user_id', async (req, res) => {
 //Delete profile or user
 router.delete('/', auth, async (req, res) => {
 	try {
+		// Remove user posts
+		await Post.deleteMany({ user: req.user.id })
 		// Remove profile
 		await Profile.findOneAndRemove({ user: req.user.id })
 		// Remove user
@@ -183,7 +186,7 @@ router.put(
 		try {
 			const profile = await Profile.findOne({ user: req.user.id })
 
-			profile.experience.unshift(newEdu)
+			profile.experience.unshift(newExp)
 			await profile.save()
 			res.json(profile)
 		} catch (err) {
